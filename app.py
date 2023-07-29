92dcdccb8364f744a3c08bef51e519b1111d3f3e
ElhamNusratSnigdha <elham.snigdha.ch@gmail.com>
2023-07-30 00:47:29 +0600

    Add player module

--- app.py
+++ app.py
@@ -18,6 +18,31 @@ class News(db.Model):
   def __repr__(self):
     return '<News %r>' % self.id
 
+class Player(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(200), nullable=False)
+  age = db.Column(db.String(200), nullable=True)
+  weight = db.Column(db.String(200), nullable=True)
+  height = db.Column(db.String(200), nullable=True)
+  nationality = db.Column(db.String(200), nullable=True)
+  jersey_no = db.Column(db.String(200), nullable=True)
+  position = db.Column(db.String(200), nullable=True)
+  quote = db.Column(db.String(200), nullable=True)
+  created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+  def __repr__(self):
+    return '<Player %r>' % self.id
+
+class Matches(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  opponent = db.Column(db.String(200), nullable=False)
+  stadium = db.Column(db.String(200), nullable=True)
+  time = db.Column(db.String(200), nullable=True)
+  created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+  def __repr__(self):
+    return '<Player %r>' % self.id
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -94,9 +119,70 @@ def newsOne(id):
     news = News.query.get_or_404(id)
     return render_template("news-one.html", news = news)
 
+
+@app.route('/admin/players',methods=['POST','GET'])
+def playersAdd():
+    if request.method == "POST":
+      name = request.form['name']
+      age = request.form['age']
+      height = request.form['height']
+      weight = request.form['weight']
+      nationality = request.form['nationality']
+      jersey_no = request.form['jersey_no']
+      position = request.form['position']
+      quote = request.form['quote']
+      new_player = Player(name=name, age=age, height=height, weight=weight, jersey_no=jersey_no, position=position, quote=quote)
+
+      try:
+        db.session.add(new_player)
+        db.session.commit()
+        return redirect('/admin/players')
+      except: 
+        return 'An error occurred'
+      pass
+    else: 
+      all_players = Player.query.order_by(Player.created_at).all()
+      return render_template("player-add.html", all_players = all_players)
+
+@app.route('/admin/players/update/<int:id>',methods=['POST','GET'])
+def playerUpdate(id):
+  player_to_update = Player.query.get_or_404(id)
+  if request.method == "POST":
+      player_to_update.name = request.form['name']
+      player_to_update.age = request.form['age']
+      player_to_update.height = request.form['height']
+      player_to_update.weight = request.form['weight']
+      player_to_update.nationality = request.form['nationality']
+      player_to_update.jersey_no = request.form['jersey_no']
+      player_to_update.position = request.form['position']
+      player_to_update.quote = request.form['quote']
+      try:
+        db.session.commit()
+        return redirect('/admin/players')
+      except: 
+        return 'An error occurred'
+  else: 
+    return render_template("player-update.html", player = player_to_update)
+
+@app.route('/admin/players/delete/<int:id>')
+def playerDelete(id):
+  player_to_delete = Player.query.get_or_404(id)
+  try:
+    db.session.delete(player_to_delete)
+    db.session.commit()
+    return redirect('/admin/players')
+  except: 
+    return 'An error occurred'
+
 @app.route('/players')
-def players():
-    return render_template("players.html")
+def allPlayers():
+    all_players = Player.query.order_by(Player.created_at).all()
+    return render_template("players.html", all_players = all_players)
+
+@app.route('/players/<int:id>')
+def playerOne(id):
+    player = Player.query.get_or_404(id)
+    return render_template("player-one.html", player = player)
 
 @app.route('/team')
 def team():
